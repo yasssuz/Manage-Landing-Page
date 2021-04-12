0f83dd04e3a3e231d585da84f117ccd497be92db
karim fanti <karimwebdevelopment@gmail.com>
2021-04-12 23:56:12 +0200

    mobile version navbar

---
+++ src/script/app.js
@@ -0,0 +1,9 @@
+const mobileToggler = document.querySelector('.header__mobile-toggler');
+
+mobileToggler.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  mobileToggler.classList.toggle('active');
+  document.getElementById('overlay').classList.toggle('active');
+  document.querySelector('.header__navigation').classList.toggle('active');
+}
