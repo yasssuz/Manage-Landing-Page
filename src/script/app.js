91bdfdd18de0b31ba19b1f43c4d92972b9013437
karim fanti <karimwebdevelopment@gmail.com>
2021-04-13 00:00:28 +0200

    mobile navbar overflow fix

--- src/script/app.js
+++ src/script/app.js
@@ -6,4 +6,5 @@ function toggleMenu() {
   mobileToggler.classList.toggle('active');
   document.getElementById('overlay').classList.toggle('active');
   document.querySelector('.header__navigation').classList.toggle('active');
+  document.querySelector('html').classList.toggle('no-scrow');
 }
